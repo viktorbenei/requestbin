37e0d3bf6a46d81bffba28115be52089e0a7a28b
Viktor Benei <viktor.benei@gmail.com>
2016-02-06 13:09:17 +0100

    MAX_RAW_SIZE config can now be set through env var

--- requestbin/config.py
+++ requestbin/config.py
@@ -10,7 +10,8 @@ FLASK_SESSION_SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", "N1BKhJLnBqLpexO
 
 BIN_TTL = 48*3600
 STORAGE_BACKEND = "requestbin.storage.memory.MemoryStorage"
-MAX_RAW_SIZE = 1024*10
+MAX_RAW_SIZE = int(os.environ.get('MAX_RAW_SIZE', 1024*10))
+print " (config) MAX_RAW_SIZE: %d" % MAX_RAW_SIZE
 IGNORE_HEADERS = []
 MAX_REQUESTS = 20
 CLEANUP_INTERVAL = 3600
